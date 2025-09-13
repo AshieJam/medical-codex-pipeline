d1e7abf3f2db0ecae60fd244572d04be22edca95
Your Name <you@example.com>
2025-09-12 20:19:56 -0400

    icd who python added

--- medical-codex-pipeline/scripts/icd10who_processor.py
+++ medical-codex-pipeline/scripts/icd10who_processor.py
@@ -9,13 +9,36 @@ columns = ['level', 'type', 'usage', 'sort', 'parent', 'code', 'display_code',
 
 df = pd.read_csv(file_path, sep=';', header=None, names=columns)
 
-output_path = 'medical-codex-pipeline\output\icd10who.csv'
+output_path = 'medical-codex-pipeline/output/icd10who.csv'
 df.to_csv(output_path, index=False)
 
 print(f"Successfully parsed {len(df)} records from {file_path}")
 print(f"Saved to {output_path}")
-print(f"\nFirst 5 rows:")
+print(f"/nFirst 5 rows:")
 print(df.head())
 
-icd_who = pd.read_csv("medical-codex-pipeline\output\icd10who.csv")
+icd_who = pd.read_csv("medical-codex-pipeline/output/icd10who.csv")
 icd_who
+
+##Searching columns
+icd_who.columns
+icd_who.icd10_code
+icd_who.detailed_title
+
+##Combination of code and description
+icd_who_draft = icd_who[['icd10_code','detailed_title']]
+icd_who_draft
+
+##Add Last Updated Date
+icd_who_draft['last_updated'] = '2025-09-12'
+
+##Rename colums to assigntment specfications
+icd_who_final = icd_who_draft.rename(columns={
+    'icd10_code': 'code',
+    'detailed_title': 'description'
+})  
+
+##Export to CSV
+file_output_path = "C:/Users/ashle/HHA-507-2025/medical-codex-pipeline/output/icd10who_final.csv"
+icd_who_final.to_csv (file_output_path, index = False)
+icd_who_final
